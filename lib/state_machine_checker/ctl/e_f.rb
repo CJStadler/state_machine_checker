9e8aed9c548a8d4536b032db41a9616bb837b4a3
Chris Stadler <chrisstadler@gmail.com>
2019-03-26 15:32:00 -0400

    Add CTL::EF

--- lib/state_machine_checker/ctl/e_f.rb
+++ lib/state_machine_checker/ctl/e_f.rb
@@ -2,17 +2,17 @@ require_relative "unary_operator"
 
 module StateMachineChecker
   module CTL
-    # The existential next operator.
-    class EX < UnaryOperator
-      # The states which have a transition directly to a state which satisfies
-      # the sub-formula.
+    # The existential eventually operator.
+    class EF < UnaryOperator
+      # The states from which a state which satisfies the sub-formula is
+      # reachable.
       #
       # @param [LabeledMachine] model
       # @return [Set<Symbol>]
       def satisfying_states(model)
         subformula
           .satisfying_states(model)
-          .flat_map { |s| model.previous_states(s) }
+          .flat_map { |s| model.predecessor_states(s) }
           .to_set
       end
     end
--- spec/state_machine_checker/ctl/e_f_spec.rb
+++ spec/state_machine_checker/ctl/e_f_spec.rb
@@ -1,47 +1,47 @@
 require "state_machine_checker/ctl/atom"
-require "state_machine_checker/ctl/e_x"
+require "state_machine_checker/ctl/e_f"
 require "state_machine_checker/labeled_machine"
 
-RSpec.describe StateMachineChecker::CTL::EX do
+RSpec.describe StateMachineChecker::CTL::EF do
   describe "#atoms" do
     it "enumerates the atoms of its subformula" do
       a1 = StateMachineChecker::CTL::Atom.new(:foo?)
       a2 = StateMachineChecker::CTL::Atom.new(:bar?)
 
       and1 = StateMachineChecker::CTL::And.new([a1, a2])
-      ex = described_class.new(and1)
+      ef = described_class.new(and1)
 
-      expect(ex.atoms).to contain_exactly(a1, a2)
+      expect(ef.atoms).to contain_exactly(a1, a2)
     end
   end
 
   describe "#satisfying_states" do
-    it "enumerates states which have a direct successor satisfying the subformula" do
+    it "enumerates states which have a successor satisfying the subformula" do
       atom = StateMachineChecker::CTL::Atom.new(:foo?)
-      ex = described_class.new(atom)
+      ef = described_class.new(atom)
 
       labels = {
-        a: [atom],
-        b: [atom],
-        c: [],
+        a: [],
+        b: [],
+        c: [atom],
         d: [],
-        e: [],
+        e: [atom],
       }
 
-      previous_states = {
+      predecessor_states = {
         a: [],
-        b: [:c, :d],
-        c: [:a],
-        d: [:b],
-        e: [:d],
+        b: [:a],
+        c: [:a, :b, :c],
+        d: [:a],
+        e: [:a, :d],
       }
 
       machine = instance_double(StateMachineChecker::LabeledMachine)
       allow(machine).to receive(:states).and_return(labels.keys)
-      allow(machine).to receive(:previous_states) { |s| previous_states[s] }
+      allow(machine).to receive(:predecessor_states) { |s| predecessor_states[s] }
       allow(machine).to receive(:labels_for_state) { |s| Set.new(labels[s]) }
 
-      expect(ex.satisfying_states(machine)).to contain_exactly(:c, :d)
+      expect(ef.satisfying_states(machine)).to contain_exactly(:a, :b, :c, :d)
     end
   end
 end
